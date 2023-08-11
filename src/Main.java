030f36d710a4542d33302bddd1fd68d617ffc5d2
Nurtik <nurtegingamer@gmail.com>
2023-08-11 21:34:45 +0300

    asd

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-    Student student1 = new Student("Red",18);
+        ArrayList<Student> students = new ArrayList<>();
+
+        try{ Student student1 = new Student("Red",18);
     Student student2 = new Student("Orange", 18);
     Student student3 = new Student("Yellow",17);
     Student student4 = new Student("Green",16);
@@ -10,7 +12,6 @@ public class Main {
     Student student6 = new Student("Blue",16);
     Student student7 = new Student("Purple",14);
     Student student8 = new Student("Black",-10);
-        ArrayList<Student> students = new ArrayList<>();
         students.add(student1);
         students.add(student2);
         students.add(student3);students.add(null);
@@ -19,7 +20,9 @@ public class Main {
         students.add(student6);
         students.add(student7);
         students.add(student8);
-
+        }catch (RuntimeException e){
+            throw new RuntimeException();
+        }
         for (Student student : students) {
             try{
             System.out.println(student);}catch (RuntimeException e){
